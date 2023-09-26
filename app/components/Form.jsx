eab05bf4cbb39e090c9b255fbc0a742731d376bb
ibby <al21760482@ite.edu.mx>
2023-09-26 12:38:31 -0700

    Cambios a Form y page

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const Form = () => {
+export const Form = ({ setResult, setError }) => {
+  const [heigth, setHeigth] = useState('');
+  const [weigth, setWeigth] = useState('');
+  const [formError, setFormError] = useState(false);
+
+  const buttonHandler = (e) => {
+    e.preventDefault();
+
+    if([heigth, weigth].includes('')) {
+      setFormError(true);
+      return
+    };
+
+    setFormError(false);
+    setResult(weigth / ((heigth/100) * (heigth/100)));
+    setError(false);
+  };
+
+  const cleanFields = () => {
+    setHeigth('');
+    setWeigth('');
+    setError(true);
+  };
+
   return (
-    <div>
-        <form className='flex flex-col items-center space-y-5 border border-gray-500 p-5 rounded-lg'>
-            <div className='flex flex-col w-full'>
-                <label className='font-bold'>Estatura</label>
-                <input type="text" placeholder='Introduzca su estatura en cm' className='bg-yellow-200 p-2 rounded-md'/>
-            </div>
-            <div className='flex flex-col w-full'>
-                <label className='font-bold'>Peso</label>
-                <input type="text" placeholder='Introduzca su peso en kg' className='bg-yellow-200 p-2 rounded-md'/>
-            </div>
-            <div className='flex w-full justify-center'>
-                <input type="button" value={'Calcular IMC'} 
-                className='bg-yellow-300 rounded-md p-2 font-semibold cursor-pointer hover:bg-yellow-500 transition ease-in-out delay-100'/>
-            </div>
-        </form>
-    </div>
+    <form className='flex flex-col items-center space-y-5 border border-gray-500 p-5 rounded-lg'>
+
+      {formError && <div className='bg-red-500 p-2 rounded-md'>
+        Error: Debes llenar todos los campos del formulario
+      </div>}
+
+      <div className='flex flex-col w-full'>
+        <label className='font-bold'>Estatura</label>
+        <input type="number" placeholder='Introduzca su estatura en cm' className='bg-yellow-200 p-2 rounded-md' value={heigth} onChange={e => setHeigth(e.target.value)} />
+      </div>
+      <div className='flex flex-col w-full'>
+        <label className='font-bold'>Peso</label>
+        <input type="number" placeholder='Introduzca su peso en kg' className='bg-yellow-200 p-2 rounded-md' value={weigth} onChange={e => setWeigth(e.target.value)} />
+      </div>
+      <div className='flex w-full justify-center space-x-3'>
+        <input type="button" value={'Calcular IMC'} onClick={buttonHandler}
+          className='bg-yellow-300 rounded-md p-2 font-semibold cursor-pointer hover:bg-yellow-500 transition ease-in-out delay-100' />
+        <input type="button" value={'Limpiar campos'} onClick={cleanFields}
+          className='bg-yellow-300 rounded-md p-2 font-semibold cursor-pointer hover:bg-yellow-500 transition ease-in-out delay-100' />
+      </div>
+    </form>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,14 +1,27 @@
+'use client';
+import { useState } from 'react';
 import { Form } from './components/Form'
 
 export default function Home() {
+  const [result, setResult] = useState('');
+  const [error, setError] = useState(true);
+
   return (
     <main className="flex flex-col items-center space-y-5 mb-11">
       <section>
         <h1 className=" uppercase font-bold text-3x1 mt-8">Indice de Masa Corporal</h1>
       </section>
-      <section className='w-[90%]'>
-        <Form/>
+      <section className='w-[80%]'>
+        <Form
+          result = {result}
+          setResult = {setResult}
+          setError = {setError} 
+        />
       </section>
+      {error === true ? '' : (<section className='font-bold text-2x1 p-2 bg-yellow-300 rounded-md'>
+        {result}
+      </section>)}
+
     </main>
   )
 }
